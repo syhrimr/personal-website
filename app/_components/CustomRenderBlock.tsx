f24cf99ff5874bf3a39a9c3d723995cb8658e732
Syahri Maulana Ramadhan <syahri.maulana@gmail.com>
2023-10-12 23:32:40 +0700

    fix: custom render of several elements - blockquote - paragraph - hyperlink - list

--- app/_components/CustomRenderBlock.tsx
+++ app/_components/CustomRenderBlock.tsx
@@ -84,6 +84,7 @@ export default function CustomRenderBlock({ params }: Props) {
             href={value?.href}
             target={target}
             rel={rel}
+            className='underline text-blue-600 hover:text-blue-800 visited:text-purple-600'
           >
             {children}
           </a>
@@ -91,19 +92,19 @@ export default function CustomRenderBlock({ params }: Props) {
       },
     },
     block: {
-      h1: ({children}) => <h1 className="text-5xl mb-6 drop-shadow font-extrabold">{children}</h1>,
-      h2: ({children}) => <h2 className="text-3xl mb-6 drop-shadow font-extrabold">{children}</h2>,
-      h3: ({children}) => <h3 className="text-2xl mb-6 drop-shadow font-extrabold">{children}</h3>,
-      // normal: ({children}) => <p className="mb-4 leading-6">{children}</p>,
-      blockquote: ({children}) => <blockquote className="border-l-purple-500">{children}</blockquote>,
+      h1: ({children}) => <h1 className="text-5xl my-5 drop-shadow font-extrabold">{children}</h1>,
+      h2: ({children}) => <h2 className="text-3xl my-5 drop-shadow font-extrabold">{children}</h2>,
+      h3: ({children}) => <h3 className="text-2xl my-5 drop-shadow font-extrabold">{children}</h3>,
+      normal: ({children}) => <p className="inline-block py-3">{children}</p>,
+      blockquote: ({children}) => <blockquote className="p-4 mt-2 mb-4 border-l-4 border-gray-300 bg-gray-50 dark:border-gray-500 dark:bg-gray-800my-3">{children}</blockquote>,
     },
     list: {
-      bullet: ({children}) => <ul className="mb-6" style={{listStylePosition: "inside",listStyleType: "disc"}}>{children}</ul>,
-      number: ({children}) => <ol className="mb-6" style={{listStylePosition: "inside", listStyleType: "decimal"}}>{children}</ol>,
+      bullet: ({children}) => <ul className="mb-2" style={{listStylePosition: "outside",listStyleType: "disc"}}>{children}</ul>,
+      number: ({children}) => <ol className="mb-2" style={{listStylePosition: "outside", listStyleType: "decimal"}}>{children}</ol>,
     },
     listItem: {
-      bullet: ({children}) => <li className="ml-4">{children}</li>,
-      number: ({children}) => <li className="ml-6">{children}</li>,
+      bullet: ({children}) => <li className="pb-2 ml-4">{children}</li>,
+      number: ({children}) => <li className="pb-2 ml-4">{children}</li>,
     }
   };
 
