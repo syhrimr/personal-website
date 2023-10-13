b8c686e51810cb8e0213f8df6ed16c86ef6f6a21
Syahri Maulana Ramadhan <syahri.maulana@gmail.com>
2023-10-13 11:31:43 +0700

    feat: add back button at blog page for mobile

--- app/(site)/blogs/[blog]/page.tsx
+++ app/(site)/blogs/[blog]/page.tsx
@@ -2,6 +2,7 @@ import { getBlog } from "@/sanity/sanity-utils";
 
 import Balancer from "react-wrap-balancer"
 import CustomRenderBlock from "@/app/_components/CustomRenderBlock";
+import Link from "next/link";
 
 type Props = {
   params: { blog: string }
@@ -13,13 +14,21 @@ export default async function BlogPage({ params }: Props) {
   
   return(
     <div className="w-full">
-      <header className="flex items-center justify-between mb-5">
+      <header className="flex flex-col justify-between">
+        <Link
+          className="block sm:hidden mb-6"
+          href={`/blogs`}
+        >
+          <span className="text-gradient-primary font-extrabold">
+            ← Back
+          </span>
+        </Link>
         <h1 className="mb-8 text-gradient-primary text-5xl drop-shadow font-extrabold">
           <Balancer>{blog.name}</Balancer>
         </h1>
       </header>
 
-      <div className="text-lg text-gray-700">
+      <div className="text-lg text-gray-700 border border-gray-600">
         <CustomRenderBlock params={blog.content} />
       </div>
     </div>
