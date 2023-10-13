92851b90de71d086d362b6b5e8aa8c445b375bd0
Syahri Maulana Ramadhan <syahri.maulana@gmail.com>
2023-10-13 23:48:35 +0700

    fix: enhance style and event in navigation item - spacing in menu navbar - add event to close the menu after clicing

--- app/_components/BurgerButton.tsx
+++ app/_components/BurgerButton.tsx
@@ -35,26 +35,28 @@ export default function BurgerButton({ navbars }: { navbars: any[] }) {
 
         <div
           id="sm-menu-container"
-          className="hidden flex-col gap-16 py-4 bg-white absolute w-full h-[100svh] top-28"
+          className="hidden flex-col gap-16 py-4 bg-white absolute w-[100svw] h-[100svh] top-28 z-10 left-[-2.5rem]"
         >
           <ul className='flex flex-col gap-4'>
             {navbars.map((navbar, index) => ( 
               navbar.type === "nav" &&
               <li
                 key={index}
+                className='px-8'
               >
                 <Link
                   href={navbar.url}
                   title={navbar.text}
-                  className="text-gradient-primary text-lg font-bold"
+                  className="block mb-4"
+                  onClick={() => toggleMenu()}
                 >
-                  {navbar.text}
+                  <span className='text-gradient-primary text-lg font-bold'>{navbar.text}</span>
                 </Link>
-                <hr className="h-px my-4 bg-gray-200 border-0"></hr>
+                <hr className="h-px mb-4 bg-gray-200 border-0"></hr>
               </li>
             ))}
           </ul>
-
+          
           <ul className='flex flex-row gap-4 justify-center'>
             {navbars.map((navbar, index) => (
               navbar.type === "sns" &&
